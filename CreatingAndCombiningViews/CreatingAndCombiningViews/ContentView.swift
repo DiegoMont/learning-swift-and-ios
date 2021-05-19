0d207a44321ab4d1ba199126175f6152a138bcc2
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-05-19 18:25:26 -0500

    Primera práctica

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Diego Montaño on 19/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      VStack {
+        MapView()
+          .ignoresSafeArea(edges: .top)
+          .frame(height:300)
+        CircleImage()
+          .offset(y: -130)
+          .padding(.bottom, -130)
+        VStack(alignment: .leading) {
+          Text("Turtle Rock")
+            .font(.title)
+          HStack {
+            Text("Joshua Tree National Park")
+            Spacer()
+            Text("California")
+          }
+          .font(.subheadline)
+          .foregroundColor(.secondary)
+          
+          Divider()
+          
+          Text("About Turtle Rock")
+            .font(.title2)
+          Text("Descriptive text goes here.")
+        }
+        .padding()
+        
+        Spacer()
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombiningViewsApp.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Diego Montaño on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
